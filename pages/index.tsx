f66910bd502074b8f6efe239ab89dddebf8a6d3e
unknown <robinkaris7@gmail.com>
2023-07-13 17:32:11 +0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+
+
+// export const Home = () => {
+//   return (
+//     <div>index</div>
+//   )
+// }
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default Home
