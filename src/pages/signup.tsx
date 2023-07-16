19627ff6076fe9260142fc48af7e6f33c227965e
unknown <robinkaris7@gmail.com>
2023-07-16 20:32:00 +0300

    made changes to button css

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -62,7 +62,7 @@ const SignUp = () => {
     <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="email@company.com" required/>
   </div>
 
-  <button type="button" className="text-white bg-Slate-Grey hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg w-full text-sm px-5 py-5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Subscribe to monthly newsletter</button>
+  <button type="button" className="text-white bg-Slate-Grey hover:bg-tomato-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-lg w-full text-sm px-5 py-5 text-center mr-2 mb-2 dark:bg-tomato-800 dark:hover:bg-tomato-800 dark:focus:ring-blue-800">Subscribe to monthly newsletter</button>
     </form>
 
 
