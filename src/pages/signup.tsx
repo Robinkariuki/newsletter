ff47f866ea945538b38a4dd96abb1883985b46ec
unknown <robinkaris7@gmail.com>
2023-07-26 13:30:46 +0300

    made changes

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -67,8 +67,8 @@ const SignUp = () => {
 </ul>
         
         <form
-        className='group'
-        noValidate
+
+        // noValidate
         >
     <div className="mb-6 mt-5">
     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
@@ -78,14 +78,14 @@ const SignUp = () => {
      id="email"
      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 invalid:[&:not(:placeholder-shown):not(:focus)]:text-red-500 invalid:[&:not(:placeholder-shown):not(:focus)]:bg-red-50 peer"
      placeholder="email@company.com" 
-     pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+    //  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
      required/>
         <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
      valid email required
   </span>
   </div>
 
-  <button type="button" className="text-white bg-Slate-Grey hover:bg-tomato-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg w-full text-sm px-5 py-5 text-center mr-2 mb-2 dark:bg-tomato-800 dark:hover:bg-tomato-800 dark:focus:ring-red-300  group-invalid:pointer-events-none group-invalid:opacity-30">Subscribe to monthly  newsletter</button>
+  <button type="submit" className="text-white bg-Slate-Grey hover:bg-tomato-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-lg w-full text-sm px-5 py-5 text-center mr-2 mb-2 dark:bg-tomato-800 dark:hover:bg-tomato-800 dark:focus:ring-red-300 ">Subscribe to monthly  newsletter</button>
     </form>
 
 
